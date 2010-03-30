86dcf7df51cbbb2395f1a9b4eeffa770f1125e98
Steve Krenzel <Sgk284@gmail.com>
2010-03-30 00:52:38 -0700

    Played around with caching and whatnot a bit

--- seafair.py
+++ seafair.py
@@ -2,13 +2,22 @@
 # Seafair - A schemaless, persistent key-value store
 # Author: Steve Krenzel
 # License: MIT (See README for license details)
+# TODO Implement no_guarantee cache
+# TODO Implement app guarantee cache
+# TODO Implement os_guarantee cache
+# TODO Move to a single db file
+# TODO Create data_path if not exists
+# TODO Move to RESTful service
+# TODO Handle out of disk space errors
 
+import os
 from os.path import exists, join
 from struct import pack, unpack, calcsize
 from hashlib import md5
 from simplejson import dumps, loads
 from time import time
 from random import shuffle
+from pdb import set_trace
 
 class Seafair:
 
@@ -16,6 +25,9 @@ class Seafair:
     fobj = None
 
     def __init__(self, filename):
+        # This tracks if we're at the end of a file
+        self.at_end = False
+
         # Set the filename to the data path
         filename = join(Seafair.data_path, filename)
 
@@ -40,7 +52,7 @@ class Seafair:
 
     def init_ptrs(self):
         # Write 64 null pointers to the start of the file
-        self.write(0, pack('Q' * 64, *([0] * 64)))
+        self.write(pack('Q' * 64, *([0] * 64)), 0)
         self.read_ptrs()
 
     def add_table(self):
@@ -54,7 +66,7 @@ class Seafair:
 
         # Add our new address to the front of our ptr list and write it
         self.ptrs = [addr] + self.ptrs
-        self.write(0, pack('Q' * len(self.ptrs), *self.ptrs))
+        self.write(pack('Q' * len(self.ptrs), *self.ptrs), 0)
 
         # Update sizes and range
         self.update_sizes_and_ranges()
@@ -67,19 +79,20 @@ class Seafair:
         zeroed_block  = chr(0) * block_size
 
         # Seek to the end of the file
-        self.fobj.seek(0, 2)
+        self.seek_end()
+
         # Write the blocks to disk
         for i in xrange(byte_cnt / block_size):
-            self.fobj.write(zeroed_block)
+            self.write(zeroed_block)
 
         # The last block won't likely be a full block, so we create a
         # special block for the last one.
         zeroed_block = chr(0) * (byte_cnt % block_size)
-        self.fobj.write(zeroed_block)
+        self.write(zeroed_block)
 
     def read_ptrs(self):
         # We read in our ptrs
-        bytes = self.read(0, calcsize('Q' * 64))
+        bytes = self.read(calcsize('Q' * 64), 0)
 
         # Filter out any pointers that point to 0
         self.ptrs = [i for i in unpack('Q' * 64, bytes) if i > 0]
@@ -109,6 +122,7 @@ class Seafair:
         key  = md5("".join(str(data[i]) for i in sorted(key_names)) + cls)
         hsh  = int(key.hexdigest(), 16)
         slot = hsh % self.ranges[0]
+        key_bytes = key.digest()
 
         # If we already wrote this data, no need to write it again
         if addr == None:
@@ -120,20 +134,19 @@ class Seafair:
             size = len(json)
 
             # Write the data to disk
-            self.fobj.write(json)
+            self.append(json)
 
         # Read in data to search
         start  = self.ptrs[0] + (slot * self.entry_sz)
-        bytes = self.read(start, self.sector)
+        bytes = self.read(self.sector, start)
 
         # Find a place in the data to insert our key. First we see if the
         # key already exists, and if so simply overwrite it. Otherwise we
         # find the first empty spot and write our data there.
-        key_bytes = key.digest()
         for b in [key_bytes, self.null_entry]:
             i = self.find_entry(b, bytes)
             if i != None:
-                self.write(start + i, key_bytes + pack('QI', addr, size))
+                self.write(key_bytes + pack('QI', addr, size), start + i)
                 break
         else:
             self.add_table()
@@ -143,30 +156,46 @@ class Seafair:
         # Grab the keys and hash them
         key  = md5("".join(str(key[i]) for i in sorted(key.keys())) + cls)
         hsh  = int(key.hexdigest(), 16)
+        slot = hsh % self.ranges[0]
         key_bytes = key.digest()
+
         for i in xrange(len(self.ptrs)):
             slot = hsh % self.ranges[i]
 
             # Read in data to search
             start  = self.ptrs[i] + (slot * self.entry_sz)
-            bytes = self.read(start, self.sector)
+            bytes = self.read(self.sector, start)
 
             i = self.find_entry(key_bytes, bytes)
             if i != None:
                 h1, h2, addr, size = unpack(self.entry_fmt,
                                             bytes[i : i + self.entry_sz])
-                return loads(self.read(addr, size))
+                return loads(self.read(size, addr))
 
-    def write(self, addr, data):
-        self.fobj.seek(addr)
+    def write(self, data, addr = None):
+        if addr != None:
+            self.fobj.seek(addr)
+            self.at_end = False
         self.fobj.write(data)
+        #self.fobj.flush()
+        #os.fsync(self.fobj.fileno())
 
-    def read(self, addr, size):
-        self.fobj.seek(addr)
+    def append(self, data):
+        self.seek_end()
+        self.write(data)
+
+    def read(self, size, addr = None):
+        if addr != None:
+            self.fobj.seek(addr)
+            self.at_end = False
         return self.fobj.read(size)
 
+    def seek_end(self):
+        if self.at_end == False:
+            self.fobj.seek(0, 2)
+
     def get_eof(self):
-        self.fobj.seek(0, 2)
+        self.seek_end()
         return self.fobj.tell()
 
     def close(self):
@@ -180,8 +209,8 @@ class Data:
     def __init__(self, **kwargs):
         if self.__class__.__db == None:
             self.__class__.__db = Seafair(self.__class__.__name__ + ".sea")
+            self.update_keys()
         self.__dict__.update(kwargs)
-        self.update_keys()
 
     def update_keys(self):
         c = self.__class__
@@ -189,6 +218,8 @@ class Data:
 
     @classmethod
     def find(cls, **kwargs):
+        if cls.__db == None:
+            cls()
         val = cls.__db.get(kwargs, cls.__name__)
         if val:
             return cls(**val)
@@ -208,20 +239,21 @@ class Data:
                 sorted(self.__dict__.items(), lambda a, b: cmp(a[0], b[0])))
         return unicode(ret + "\n")
 
-class Phonebook(Data):
+class Twitter(Data):
     name = True
-    phone = None
+    tweet = None
 
 if __name__ == "__main__":
     o = 0
-    n = 300000
+    n = 3000
     t = time()
+    tweet = "2" * 140
     for i in xrange(o, n):
-        Phonebook(name=i, phone=i).save()
+        Twitter(name=i, tweet=tweet).save()
     print time() - t
     t = time()
     for i in xrange(o, n):
-        p = Phonebook.find(name=i)
-        if not p:
+        p = Twitter.find(name=i)
+        if not p or p.tweet != tweet or p.name != i:
             print "UGH ", i
     print time() - t
